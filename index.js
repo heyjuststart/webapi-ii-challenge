78f7479646c15f05f2bedb14cc0077fab81fbd54
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-03-19 16:05:04 -0400

    add cors usage

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 
 const postsRouter = require('./posts/posts-router');
 
 const server = express();
+server.use(cors());
 
 server.use('/', (req, res) => res.send('API up and running!'));
 server.use('/api/posts', postsRouter);
